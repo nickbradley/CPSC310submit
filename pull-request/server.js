b079769083e7d485ed60c12b1540c1c4a5470752
Nick Bradley <nickbradley@live.ca>
2016-07-19 21:29:36 -0700

    Test

--- pull-request/server.js
+++ pull-request/server.js
@@ -3,5 +3,10 @@ var Queue = require('bull');
 // 'redis' should be defined in /etc/hosts by Docker
 var jobQueue = Queue('CPSC310 Test Job Queue', '6379', 'redis');
 
+jobQueue.process(function(opts, done){
+  console.log(opts);
+});
+
+jobQueue.add("****////HELLO////****");
 
 console.log(process.env);
