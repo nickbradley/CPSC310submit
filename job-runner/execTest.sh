689bbfc0a3390992452d1b3d9bd432d1bd5559b4
Nick Bradley <nickbradley@live.ca>
2016-07-24 12:20:59 -0700

    Test

--- job-runner/execTest.sh
+++ job-runner/execTest.sh
@@ -23,8 +23,8 @@ mkdir -p "${STUDENT_REPO}" && cd "$_" && git clone ${STUDENT_REPO_URL} "$_"
 echo "*** Begin test output ***"
 
 #docker run cpsc310/tester -v "${TEST_REPO}":/test -v "${STUDENT_REPO}":/src
-docker run -v /var/run/docker.sock:/var/run/docker.sock fedora
-
+#docker run -v /var/run/docker.sock:/var/run/docker.sock fedora
+sudo docker run hello-world
 echo "*** End test output ***"
 
 rm -rf "${STUDENT_REPO}"
