06a8a4981a1b974544542bef65b08ee29fc43f21
Nick Bradley <nickbradley@live.ca>
2016-07-27 08:32:39 -0700

    Test

--- job-runner/execTest.sh
+++ job-runner/execTest.sh
@@ -39,7 +39,7 @@ git checkout -b pullrequest FETCH_HEAD
 echo "*** Begin test output ***"
 ls -l "${TEST_REPO}"
 ls -l "${STUDENT_REPO}"
-echo docker run -v repo-data:/repos cpsc310/tester
+echo docker run -v cpsc310-repo-store:/repos cpsc310/tester
 echo docker run -v "${TEST_REPO}":/test -v "${STUDENT_REPO}":/src cpsc310/tester
 docker run -v "${TEST_REPO}":/test -v "${STUDENT_REPO}":/src cpsc310/tester
 #docker run -v /var/run/docker.sock:/var/run/docker.sock fedora
