5bc93ad61007b1da50118cd2fd258c4bdbd84e78
Nick Bradley <nickbradley@live.ca>
2016-07-30 08:35:35 -0700

    Test

--- job-runner/execTest.sh
+++ job-runner/execTest.sh
@@ -25,16 +25,16 @@ STUDENT_REPO=/repos/src$(mktemp -d)
 
 
 # Clone the students repository
-#REGEX="https://api.github.com/repos/(.*?)/(.*?)/pulls/([0-9]+)"
-#if [[ ${STUDENT_REPO_URL} =~ $REGEX ]]
-#then
-#  USER_NAME="${BASH_REMATCH[1]}"
-#  REPO_NAME="${BASH_REMATCH[2]}"
-#  PULL_REQUEST="${BASH_REMATCH[3]}"
-#else
-#  echo "${STUDENT_REPO_URL} is not in the correct input format."
-#  exit 1
-#fi
+REGEX="https://api.github.com/repos/(.*?)/(.*?)/pulls/([0-9]+)"
+if [[ ${STUDENT_REPO_URL} =~ $REGEX ]]
+then
+  USER_NAME="${BASH_REMATCH[1]}"
+  REPO_NAME="${BASH_REMATCH[2]}"
+  PULL_REQUEST="${BASH_REMATCH[3]}"
+else
+  echo "${STUDENT_REPO_URL} is not in the correct input format."
+  exit 1
+fi
 
 
 
