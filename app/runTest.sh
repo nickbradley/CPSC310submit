7ad53f39127cc97f9b942f75a9762140d2230398
Nick Bradley <nickbradley@live.ca>
2016-08-09 10:04:08 -0700

    test

--- app/runTest.sh
+++ app/runTest.sh
@@ -45,6 +45,7 @@ echo "*** Begin test output ***"
 #ls "${STUDENT_REPO}"
 #docker run -td -v "${TEST_REPO}":/test:z -v "${STUDENT_REPO}":/src:z cpsc310/tester
 ls -l /var/run
+ls -l /run
 docker ps
 echo "Docker exit status $?"
 #cat "${TEST_REPO}"/results.json
