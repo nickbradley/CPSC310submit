f775104ebe971548959142dd2ffef7c89a7928e3
Nick Bradley <nickbradley@live.ca>
2016-08-09 20:55:01 -0700

    test

--- app/runTest.sh
+++ app/runTest.sh
@@ -31,10 +31,10 @@ fi
 
 
 # Set the test repository directory based on the pull request
-case "${REPO_NAME}" in
-"Test") TEST_REPO=/repos/test ;;
-esac
-#TEST_REPO=/repos/test 
+#case "${REPO_NAME}" in
+#"Test") TEST_REPO=/repos/test ;;
+#esac
+TEST_REPO=/repos/test
 # Clone the test repo if it doesn't already exist
 if [[ ! -d "${TEST_REPO}" ]]
 then
