2a277a8557853ce2c47934e6192e205d29ed4eb4
nickbradley <nickbradley@live.ca>
2016-09-20 21:28:28 -0700

    git command

--- app.sh
+++ app.sh
@@ -91,9 +91,9 @@ then
       (>&2 echo "Updating test repo")
       git config user.name "cpsc310bot"
       git config user.email "cpsc310bot@gmail.com"
-      git add *
+      git add .git/config
       git commit -m "some init msg"
-      
+
       (>&2 echo "Updating test repo1")
       git pull #-c user.email="cpsc310bot@gmail.com" -c user.name="cpsc310bot"
       (>&2 echo "Updating test repo2")
