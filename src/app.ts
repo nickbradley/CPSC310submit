0b6d3569a4e547d442d6eb8c9fcc4c67bac383c6
nickbradley <nickbradley@live.ca>
2016-10-12 19:59:47 -0700

    Change output formatting

--- src/app.ts
+++ src/app.ts
@@ -366,7 +366,7 @@ function formatTestReport(testReport: any): string {
       let test: string = failedTest.fullTitle;
       let testCodeName: string = test.substring(test.indexOf("~")+1, test.lastIndexOf("~"));
       //output += "\n\t - " + test.substring(0, test.indexOf(" \n\t["));
-      output += "\n* " + testCodeName + " *" + test.substring(test.lastIndexOf("~")+1, test.indexOf(".")+1) + "*";
+      output += "\n* " + test.substring(test.lastIndexOf("~")+1, test.indexOf(".")+1) + " (" + testCodeName + ")";
     });
     //let failedTests: any[] = testReport.allTests.filter((test: any) => {
     //  return test.fail;
