a9337108a9590e27b874d5ae29b6c98470ecd485
nickbradley <nickbradley@live.ca>
2016-11-13 11:29:17 -0800

    Set rate limit to 6 hours

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ var AppSetting = {
     },
     requestLimit: {
         maxCount: process.env.MAX_REQUESTS || 10,
-        minDelay: process.env.MIN_REQUEST_DELAY || 43200000
+        minDelay: process.env.MIN_REQUEST_DELAY || 21600000
     },
     cmd: {
         concurrency: process.env.WORKERS || 1,
